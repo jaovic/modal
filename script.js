320a5bdcf152299355ca982e0480f73b27c323c5
João Victor <jvmsf05@gmail.com>
2021-12-21 01:07:03 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,25 @@
+function iniciaModal(modalID) {
+
+    //if(localStorage.fechaModal !== modalID) {
+        const modal = document.getElementById(modalID);
+        if(modal) {
+            modal.classList.add('mostrar');
+            modal.addEventListener('click', (e) => {
+                if(e.target.id == modalID || e.target.className == 'fechar') {
+                    modal.classList.remove('mostrar');
+                    localStorage.fechaModal = modalID;
+                }
+            });
+        }
+    }
+    //}
+
+    const logo = document.querySelector('.btn');
+    logo.addEventListener('click', () => iniciaModal('modal-promocao'));
+
+    document.addEventListener('scroll', () => {
+        
+        if(window.pageYOffset > 800) {iniciaModal('modal-promocao');
+
+    }
+    })
